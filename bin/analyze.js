bf33445e825e9d4b886ecf3d7c4d09c382cdca22
Marat Dulin <mdevils@yandex.ru>
2013-11-12 20:31:34 +0400

    WIP

--- bin/analyze.js
+++ bin/analyze.js
@@ -1,25 +1,33 @@
 #!/usr/bin/env node
 var program = require('commander');
 var Builder = require('../lib/builder');
-var PlantUml = require('../lib/puml');
 
-program.command('puml')
-    .description('build PlantUML')
-    .option('--scope <dir>', 'process directory')
-    .action(function() {
-        var builder = new Builder();
-        builder.buildProject(process.cwd(), args.scope).then(function (project) {
-            var plantUml = new PlantUml();
-            console.log(plantUml.exportToPlantUml(project));
-        }).done();
-    });
-
-program.command('validate')
-    .option('--scope <dir>', 'process directory')
+program.command('report')
+    .description('report scheme info')
+    .option('-r, --reporter <reporter>', 'reporter (puml, dump, errors)')
+    .option('-s, --scope <dir>', 'process directory')
+    .option('-f, --filter <filter>')
+    .option('-v, --validator <validator>')
     .action(function(args) {
         var builder = new Builder();
-        builder.buildProject(process.cwd(), args.scope).then(function () {
-            builder.getErrorLog().report();
+        builder.buildProject(process.cwd(), args.scope).then(function (project) {
+            if (args.filter) {
+                args.filter.split(':').forEach(function (filterStr) {
+                    var filterParts = filterStr.split('=');
+                    var filterName = filterParts.shift();
+                    var filterValue = filterParts.shift();
+                    project = require('../lib/filters/' + filterName)(project, filterValue);
+                });
+            }
+            if (args.validator) {
+                args.validator.split(':').forEach(function (validatorStr) {
+                    var validatorParts = validatorStr.split('=');
+                    var validatorName = validatorParts.shift();
+                    var validatorValue = validatorParts.shift();
+                    require('../lib/validators/' + validatorName)(project, builder.getErrorLog(), validatorValue);
+                });
+            }
+            require('../lib/reporters/' + (args.reporter || 'dump'))(project, builder.getErrorLog());
         }).done();
     });
 
